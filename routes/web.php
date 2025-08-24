78833ec7d21a5499d3e16747169e5f7083d9bbf0
Bosnape <pcabrejosm@gmail.com>
2025-08-23 23:40:45 -0500

    Tutorial 03 + Login

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
+// Home routes
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 Route::get('/about', function () {
     $data1 = 'About us - Online Store';
@@ -20,10 +15,26 @@ Route::get('/about', function () {
         ->with('description', $description)
         ->with('author', $author);
 })->name('home.about');
-
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
+// Product routes
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+// Cart routes
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
+
+// Image routes
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+
+// Image not DI routes
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
+
+// Auth routes
+Auth::routes();
